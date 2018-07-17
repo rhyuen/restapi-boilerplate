c8e490c2bcb1c0bd8645bcfa7681f898415ec8f9
Robert Yuen <dungeonry@hotmail.com>
2018-07-16 19:01:29 -0700

    Made Controller folder.  Fixed PORT value in index.js and mispellings in index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const server = require("./server.js");
 const winston = require("winston");
-const PORT = process.env.NODE_ENV || 9934;
+const PORT = process.env.PORT || 9934;
 
 process.on("uncaughtException", (err) => {    
-    winston.log("error", `Uncaught Exception: ${e}`);    
+    winston.log("error", `Uncaught Exception: ${err}`);    
     process.exit(1);    
 });
 
 process.on("uncaughtRejection" , (err, promise) => {
-    winston.log("error", `Uncaught Rejection: ${e}`);    
+    winston.log("error", `Uncaught Rejection: ${err}`);
     process.exit(1);
 });
 
 server.listen(PORT, (err) => {    
-    if(err) return console.log(err);    
-    console.log(`Generic Rest Service: PORT ${PORT}`);        
+    if(err) {
+        return console.log(err);    
+    }
+    console.log(`Generic Rest Service in \'${process.env.NODE_ENV}\' environment : PORT ${PORT}`);    
 });
--- server.js
+++ server.js
@@ -3,23 +3,11 @@ const winston = require("winston");
 const app = express();
 const wrapAsync = require("./common/util.js");
 const middleware = require("./common/middleware.js");
+const unauthedRoutes = require("./routes/unauthedRoutes.js");
 
 middleware(app);
 
-app.get("/", wrapAsync(async (req, res) => {        
-    res.status(200).json({
-            route: "/",
-            message: "Welcome to my generic service.",
-            routes: "Routes available are /"
-    });    
-}));
-
-app.get("*", wrapAsync(async (req, res, next) => {    
-    res.status(404).json({
-        route: "notfound",
-        message: "This route doesn't exist"
-    });
-}));
+app.use("/", unauthedRoutes);
 
 app.use((err, req, res, next) => {        
     winston.log("error", `App caught an error: ${e}`);
