036ac655cddf3044ef294bdf81f376e61b1905ff
Robert Yuen <dungeonry@hotmail.com>
2018-10-22 02:48:07 -0700

    removing hard coded port from npm scripts for deployment to openshift.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const server = require("./server.js");
 const logger = require("./common/logger.js");
-const PORT = process.env.PORT || 9934;
+const PORT = process.env.PORT || 8080;
 
-process.on("uncaughtException", (err) => {    
+process.on("uncaughtException", (err) => {
     logger.error(err);
-    process.exit(1);    
+    process.exit(1);
 });
 
-server.listen(PORT, (err) => {    
-    if(err) {        
-        return console.log(err);    
-    }    
-    console.log(`Generic Rest Service in \'${process.env.NODE_ENV}\' environment : PORT ${PORT}`);    
+server.listen(PORT, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(`Generic Rest Service in \'${process.env.NODE_ENV}\' environment : PORT ${PORT}`);
 });
